7df2253b364f9b85be347d8a404663e817f63007
xjy <xjy1907@163.com>
2022-02-27 12:17:58 +0800

    解决了Safari浏览器强制缩放问题

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -150,6 +150,32 @@ var step = function step() {
 };
 
 step();
+/**
+ * 禁止浏览器强制缩放
+ */
+
+window.onload = function () {
+  // 阻止双击放大
+  var lastTouchEnd = 0;
+  document.addEventListener('touchstart', function (event) {
+    if (event.touches.length > 1) {
+      event.preventDefault();
+    }
+  });
+  document.addEventListener('touchend', function (event) {
+    var now = new Date().getTime();
+
+    if (now - lastTouchEnd <= 300) {
+      event.preventDefault();
+    }
+
+    lastTouchEnd = now;
+  }, false); // 阻止双指放大
+
+  document.addEventListener('gesturestart', function (event) {
+    event.preventDefault();
+  });
+};
 },{}],"../../../AppData/Local/Yarn/Data/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -178,7 +204,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "7150" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "8813" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
